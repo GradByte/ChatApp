71320a0bb8b50d7e20db6ddd1f5cb2b116815f7b
GradByte <eertekin99@outlook.com>
2024-01-23 14:49:02 +0300

    project creation

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by GradByte on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
